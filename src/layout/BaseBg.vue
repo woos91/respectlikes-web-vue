ab7da5d5439e97a9a2f6928c3467e08f53210127
woos91 <wooos91@gmail.com>
2024-11-18 18:22:46 +0900

    Update build

---
+++ src/layout/BaseBg.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+    <div 
+        class="home-bg" 
+        @style="`background:linear-gradient(135deg, rgba(${bgColor.color1.value},1) 0%, rgba(${bgColor.color2.value},1) 47%, rgba(${bgColor.color3.value},1) 100%)`">
+    </div>
+</template>
+
+<style scoped>
+    .home-bg {
+        z-index:-1;
+        position: fixed;
+        left:0;
+        top:0;
+        right:0;
+        bottom:0;
+        opacity:0;
+        animation-name: cast-in;
+        animation-duration: 4s;
+        animation-timing-function: linier;
+        animation-delay: 0.2s;
+        animation-fill-mode: forwards;
+	}
+
+@media (min-width: 1024px) {
+    
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import axios from 'axios';
+
+const app = createApp(App)
+
+app.config.globalProperties.axios = axios;
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
